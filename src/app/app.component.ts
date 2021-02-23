5ca5e1e6250929fd1787099d077778ff42dc8ebb
WellyX4869 <wellysusanto4869@gmail.com>
2021-02-23 19:27:31 +0700

    Commit Pertama - Hasil Awal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,114 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  board = [
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"]
+  ];
+
+  user: String = "1";
+  baris = 0;
+  kolom = 0;
+  validitas = "";
+  hasil = "";
+
+  ChangeTurn() {
+    var turn = Number(this.user) % 2;
+    console.log(turn);
+    var valid = this.CheckValid();
+    if (valid == 1) {
+      this.validitas = "";
+      this.board[this.baris][this.kolom] = turn.toString();
+
+      var res = this.CheckWin();
+      if (res == 0) {
+        this.ChangeUser();
+      }
+      else{
+        this.hasil = "Selamat, User "+this.user+" menang";
+      }
+    } else {
+      this.validitas = "*Baris atau kolom tidak valid ATAU sudah terisi";
+    }
+  }
+
+  CheckValid() {
+    if (
+      (this.baris >= 5 && this.baris < 0) ||
+      (this.kolom >= 5 && this.kolom < 0)
+    ) {
+      console.log("jancok1");
+      return 0;
+    }
+    if (
+      this.board[this.baris][this.kolom] == "0" ||
+      this.board[this.baris][this.kolom] == "1"
+    ) {
+      console.log("jancok2");
+      return 0;
+    }
+    return 1;
+  }
+
+  CheckWin() {
+    for (let i = 0; i < this.board.length; i++) {
+      //check horizontal
+      // check front
+      if (
+        this.board[i][0] == this.board[i][1] &&
+        this.board[i][1] == this.board[i][2] &&
+        this.board[i][2] == this.board[i][3] && 
+        this.board[i][0] != "*"
+      ) {
+        return 1;
+      }
+      // check back
+      if (
+        this.board[i][1] == this.board[i][2] &&
+        this.board[i][2] == this.board[i][3] &&
+        this.board[i][3] == this.board[i][4] &&
+        this.board[i][1] != "*"
+      ) {
+        return 1;
+      }
+
+      //check vertical
+      // check front
+      if (
+        this.board[0][i] == this.board[1][i] &&
+        this.board[1][i] == this.board[2][i] &&
+        this.board[2][i] == this.board[3][i] && 
+        this.board[0][i] != "*"
+      ) {
+        return 1;
+      }
+      // check back
+      if (
+        this.board[1][i] == this.board[2][i] &&
+        this.board[2][i] == this.board[3][i] &&
+        this.board[3][i] == this.board[4][i] &&
+        this.board[1][i] != "*"
+      ) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  ChangeUser() {
+    if (this.user == "1") {
+      this.user = "2";
+    } else {
+      this.user = "1";
+    }
+  }
 }
